73fe1d0c072aff42a7746cca87eba4532fb17238
Dawson <dragon520111@gmail.com>
2025-02-10 13:20:29 -0500

    Worked on huffman encoding implemetation in python

--- Data Structures/huff.py
+++ Data Structures/huff.py
@@ -1,17 +1,20 @@
 import heapq
 
 class Node:
-    def __init__(self, freq : int, char):
+    def __init__(self, freq = 0, char = ''):
         self.freq = freq
         self.char = char
         self.left = None
         self.right = None
 
     def __lt__(self, other):
-        return self.freq < other.freq
+        return self.freq <= other.freq
 
     def __repr__(self):
-        return f"Node(freq={self.freq},char={self.char})"
+        left_repr = f"left={repr(self.left)}" if self.left else "left=None"
+        right_repr = f"right={repr(self.right)}" if self.right else "right=None"
+        return f"Node(freq={self.freq}, char={self.char}, {left_repr}, {right_repr})"
+
 
 
 class NodeHeap:
@@ -19,19 +22,55 @@ class NodeHeap:
         self.nodes = []
     
     def insert(self, node : Node):
-        self.nodes.append(node)
-        heapq.heapify(self.nodes)
+        heapq.heappush(self.nodes, node)
 
     def extractMin(self):
         return heapq.heappop(self.nodes)
+    
+    def clearNodes(self):
+        self.nodes.clear()
 
     def __repr__(self):
         return self.nodes.__repr__()
 
+    def __len__(self):
+        return len(self.nodes)
+    
+
+class HuffmanTree:
+    def __init__(self, characters = ""):
+        self.characters = characters
+
+    def buildHeap(self, nodeHeap : NodeHeap):
+        wordFreq = {}
+
+        for i in self.characters:
+            wordFreq[i] = wordFreq.setdefault(i, 0) + 1
+        
+        for key, value in wordFreq.items():
+            nodeHeap.insert(Node(value, key))
+        
+    
+    def buildTree(self, nodeHeap: NodeHeap):
+        while len(nodeHeap) > 1:
+            minNode1 = nodeHeap.extractMin()
+            minNode2 = nodeHeap.extractMin()
+
+            newRoot = Node(minNode1.freq + minNode2.freq)
+            newRoot.left = minNode1
+            newRoot.right = minNode2
+
+            nodeHeap.insert(newRoot)
+        
+        return nodeHeap.extractMin()
+            
 
 if __name__ == "__main__":
     nh = NodeHeap()
-    nh.insert(Node(5, 'c'))
-    nh.insert(Node(5, 'b'))
-    nh.insert(Node(2, 'e'))
-    print(nh)
+    hf = HuffmanTree("foo bar is cool")
+    hf.buildHeap(nh)
+    root = hf.buildTree(nh)
+    print(root)
+    
+
+
