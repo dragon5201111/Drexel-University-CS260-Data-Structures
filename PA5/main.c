1ca7ac5a9d41b3f19b10ab078b12a5da19f04f27
Dawson <dragon520111@gmail.com>
2025-03-08 10:53:43 -0500

    Implemented queue and refactored code.

--- PA5/main.c
+++ PA5/main.c
@@ -8,6 +8,119 @@ typedef struct SlidingPuzzle{
 	struct SlidingPuzzle * predecessor_puzzle;
 }SlidingPuzzle;
 
+
+typedef struct PuzzleQueueNode{
+	SlidingPuzzle * puzzle;
+	struct PuzzleQueueNode * next;
+}PuzzleQueueNode;
+
+typedef struct PuzzleQueue{
+	PuzzleQueueNode * head;
+	PuzzleQueueNode * tail;
+}PuzzleQueue;
+
+// Queue Functions
+/*
+* Creates a new PuzzleQueue.
+* Returns: 
+*     - PuzzleQueue pointer if the queue is successfully created
+*     - NULL if memory allocation fails.
+*/
+PuzzleQueue * create_puzzle_queue() {
+    PuzzleQueue * puzzle_queue;
+    if((puzzle_queue = (PuzzleQueue *) malloc(sizeof(PuzzleQueue))) == NULL){
+        return NULL;
+    }
+    puzzle_queue->head = puzzle_queue->tail = NULL;
+    return puzzle_queue;
+}
+
+/*
+* Creates a new PuzzleQueueNode.
+* Assumes puzzle is non-NULL.
+* Returns:
+*     - PuzzleQueueNode pointer if successful, otherwise NULL.
+*/
+PuzzleQueueNode * create_puzzle_queue_node(SlidingPuzzle * puzzle) {
+	if (puzzle == NULL) {
+        return NULL;
+    }
+
+    PuzzleQueueNode * puzzle_queue_node;
+    if((puzzle_queue_node = (PuzzleQueueNode *) malloc(sizeof(PuzzleQueueNode))) == NULL) {
+        return NULL;
+    }
+    puzzle_queue_node->next = NULL;
+    puzzle_queue_node->puzzle = puzzle;
+    return puzzle_queue_node;
+}
+
+/*
+Assumes puzzle_queue and puzzle_queue_node are non-NULL
+Returns:
+    - PuzzleQueue* on success, otherwise NULL
+*/
+PuzzleQueue * enqueue_puzzle_queue_node(PuzzleQueue * puzzle_queue, PuzzleQueueNode * puzzle_queue_node) {
+    if (puzzle_queue == NULL || puzzle_queue_node == NULL) {
+        return NULL; // Sanity check
+    }
+
+	if (puzzle_queue->head == NULL) {
+        puzzle_queue->head = puzzle_queue->tail = puzzle_queue_node;
+    } else {
+        puzzle_queue->tail->next = puzzle_queue_node;
+        puzzle_queue->tail = puzzle_queue_node;
+    }
+    return puzzle_queue;
+}
+
+/*
+Assumes puzzle_queue is non-NULL
+Returns:
+    - PuzzleQueueNode* on success, NULL on failure
+*/
+PuzzleQueueNode * dequeue_puzzle_queue_node(PuzzleQueue * puzzle_queue){
+	if (puzzle_queue == NULL || puzzle_queue->head == NULL) {
+        return NULL;
+    }
+	
+	PuzzleQueueNode * dequeued_node = puzzle_queue->head;
+	puzzle_queue->head = puzzle_queue->head->next;
+
+	if(puzzle_queue->head == NULL){
+		puzzle_queue->tail = NULL;
+	}
+
+	return dequeued_node;
+}
+
+void free_puzzle_queue(PuzzleQueue * puzzle_queue) {
+    PuzzleQueueNode * current = puzzle_queue->head;
+    while (current != NULL) {
+        PuzzleQueueNode * next_node = current->next;
+        free(current);
+        current = next_node;
+    }
+    free(puzzle_queue);
+}
+
+int puzzle_queue_is_empty(PuzzleQueue * puzzle_queue){
+	return (puzzle_queue->head == NULL && puzzle_queue->tail == NULL);
+}
+
+// File Functions
+int open_input_and_output_file(FILE ** input_file, FILE ** output_file, char ** argv){
+	*input_file = fopen(argv[1], "r");
+	*output_file = fopen(argv[2], "w");
+	return (*input_file != NULL || *output_file != NULL);
+}
+
+void close_input_and_output_file(FILE * input_file, FILE * output_file){
+	fclose(input_file);
+	fclose(output_file);
+}
+
+// Puzzle Functions
 /*
 Returns:
 	1 if cannot be solved, otherwise, 0
@@ -96,29 +209,65 @@ void free_puzzle(SlidingPuzzle *puzzle) {
     free(puzzle);
 }
 
-int open_input_and_output_file(FILE ** input_file, FILE ** output_file, char ** argv){
-	*input_file = fopen(argv[1], "r");
-	*output_file = fopen(argv[2], "w");
-	return (*input_file != NULL || *output_file != NULL);
+int row_and_column_in_puzzle_bounds(int row, int column, int k){
+	return (row >= 0 && column >=0 && row < k && column < k);
 }
 
-void close_input_and_output_file(FILE * input_file, FILE * output_file){
-	fclose(input_file);
-	fclose(output_file);
+/*	
+	First index is up, second is down, third is left, fourth is right
+	[-1,-1,-1,-1]
+	-1 at an index indicates there is no neighbor for that position respectively
+*/
+void get_puzzle_neighbors(SlidingPuzzle * puzzle, int i, int * neighbor_array){
+	int k = puzzle->k;
+	int row = i / k;
+	int column = i % k;
+
+	// up
+	neighbor_array[0] = row_and_column_in_puzzle_bounds(row-1, column, k) ? (row - 1) * k + column : -1; 
+	// down
+	neighbor_array[1] = row_and_column_in_puzzle_bounds(row+1, column, k) ? (row + 1) * k + column : -1; 
+	// left
+	neighbor_array[2] = row_and_column_in_puzzle_bounds(row, column-1, k) ? row * k + (column - 1) : -1; 
+	// right
+	neighbor_array[3] = row_and_column_in_puzzle_bounds(row, column+1, k) ? row * k + (column + 1) : -1; 
 }
 
-void _print_puzzle(SlidingPuzzle *puzzle) {
-    if (puzzle == NULL){
-        return;
+void get_zero_neighbors(SlidingPuzzle * puzzle, int * neighbor_array){
+	int zero_index = -1;
+	int k = puzzle->k;
+
+	for(int i = 0; i < k*k; i++){
+		if(puzzle->board[i] == 0){
+			zero_index = i;
+			break;
+		}
 	}
 
+	get_puzzle_neighbors(puzzle, zero_index, neighbor_array);
+}
+
+int puzzles_are_the_same(SlidingPuzzle * puzzle_one, SlidingPuzzle * puzzle_two){
+	if(puzzle_one == NULL || puzzle_two == NULL) return 0;
+	// Boards will not have different sizes so, k will suffice
+	int k = puzzle_one->k;
+	return memcmp(puzzle_one->board, puzzle_two->board, sizeof(int) * k * k) == 0;
+}
+
+void _print_puzzle_statistics(SlidingPuzzle *puzzle){
 	printf("======================================\n");
     printf("Puzzle Statistics:\n======================================\n* Size -> %d x %d\n* Has Predecessor -> %s\n* Is Solvable -> %s\n", 
         puzzle->k, puzzle->k, 
         (puzzle->predecessor_puzzle == NULL) ? "No (NULL)" : "Yes",
 		(!is_puzzle_unsolvable(puzzle)) ? "Yes" : "No");
+}
 
-    printf("======================================\n");
+void _print_puzzle_board(SlidingPuzzle *puzzle){
+	if(puzzle == NULL){
+		return;
+	}
+
+	printf("======================================\n");
 	printf("Game Board:\n");
 	printf("======================================\n");
 
@@ -157,43 +306,33 @@ void _print_puzzle(SlidingPuzzle *puzzle) {
         if ((i + 1) % k == 0)
             putchar('\n');
     }
+}
+
+void _print_zero_neighbors(SlidingPuzzle *puzzle){
 	printf("======================================\n");
 	printf("Zero Neighbors:\n");
 	printf("======================================\n");
 
 	int zero_neighbors [4];
-	get_puzzle_neighbors(puzzle, zero_index, zero_neighbors);
+	get_zero_neighbors(puzzle, zero_neighbors);
 	
 	printf("Up Neighbor[%d] = \033[38;5;214m%d\033[0m\n", zero_neighbors[0], (zero_neighbors[0] == -1) ? -1 : puzzle->board[zero_neighbors[0]]);
 	printf("Down Neighbor[%d] = \033[38;5;214m%d\033[0m\n", zero_neighbors[1], (zero_neighbors[1] == -1) ? -1 : puzzle->board[zero_neighbors[1]]);
 	printf("Left Neighbor[%d] = \033[38;5;214m%d\033[0m\n", zero_neighbors[2], (zero_neighbors[2] == -1) ? -1 : puzzle->board[zero_neighbors[2]]);
 	printf("Right Neighbor[%d] = \033[38;5;214m%d\033[0m", zero_neighbors[3], (zero_neighbors[3] == -1) ? -1 : puzzle->board[zero_neighbors[3]]);
-	printf("\n======================================\n");
+	printf("\n======================================\n\n");
 }
 
-int row_and_column_in_puzzle_bounds(int row, int column, int k){
-	return (row >= 0 && column >=0 && row < k && column < k);
+void _print_puzzle(SlidingPuzzle *puzzle) {
+    if (puzzle == NULL){
+        return;
+	}
+
+	_print_puzzle_statistics(puzzle);
+    _print_puzzle_board(puzzle);
+	_print_zero_neighbors(puzzle);
 }
 
-/*	
-	First index is up, second is down, third is left, fourth is right
-	[-1,-1,-1,-1]
-	-1 at an index indicates there is no neighbor for that position respectively
-*/
-void get_puzzle_neighbors(SlidingPuzzle * puzzle, int i, int * neighbor_array){
-	int k = puzzle->k;
-	int row = i / k;
-	int column = i % k;
-
-	// up
-	neighbor_array[0] = row_and_column_in_puzzle_bounds(row-1, column, k) ? (row - 1) * k + column : -1; 
-	// down
-	neighbor_array[1] = row_and_column_in_puzzle_bounds(row+1, column, k) ? (row + 1) * k + column : -1; 
-	// left
-	neighbor_array[2] = row_and_column_in_puzzle_bounds(row, column-1, k) ? row * k + (column - 1) : -1; 
-	// right
-	neighbor_array[3] = row_and_column_in_puzzle_bounds(row, column+1, k) ? row * k + (column + 1) : -1; 
-}
 
 int main(int argc, char **argv){
 	if(argc != 3){
@@ -214,17 +353,26 @@ int main(int argc, char **argv){
 		return EXIT_FAILURE;
 	}
 
+	// Debug print
 	_print_puzzle(initial_puzzle);
+
+	// Create queue
+	PuzzleQueue * puzzle_queue;
+	if((puzzle_queue = create_puzzle_queue()) == NULL){
+		fprintf(stderr, "Unable to allocate memory for puzzle queue.\n");
+		free_puzzle(initial_puzzle);
+		close_input_and_output_file(input_file, output_file);
+		return EXIT_FAILURE;
+	}
 
 	/*
 	TODO:
-		0.) Add puzzle indexing operations
-		1.) Implement puzzle queue
+		1.) Implement Hash Table to keep track of created boards
 		2.) Implement BFS
 		3.) Output solution
 	*/
-
 	free_puzzle(initial_puzzle);
+	free_puzzle_queue(puzzle_queue);
 	close_input_and_output_file(input_file, output_file);
 	return 0;
 }
